c4f51d8666d090f47c0c8a5799da88d61deb3258
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-20 21:58:00 +0200

    fix

---
+++ APIFeatures/ErrorHandler.js
@@ -0,0 +1,14 @@
+
+
+class ErrorHandler extends Error{
+  constructor(message,statusCode) {
+    super(message)
+    this.message = message;
+    this.statusCode = statusCode;
+    this.status = `${this.statusCode}`.startsWith('4') ? 'fail' : "error";
+
+    this.isOperational = true;
+  }
+}
+
+module.exports = ErrorHandler;
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 const PORT =  process.env.PORT || 3000
 
+
 mongoose.connect("mongodb+srv://temcenkovova8:brFMAZAjzkX4ighR@cluster0.4dgfzzn.mongodb.net/natours?retryWrites=true&w=majority",{
   useNewUrlParser:true,
   useCreateIndex:true,
@@ -15,7 +16,26 @@ mongoose.connect("mongodb+srv://temcenkovova8:brFMAZAjzkX4ighR@cluster0.4dgfzzn.
 
 const pizzaRouter = require('./Routers/Pizza/PizzaRouts')
 const socialRouter = require('./Routers/SocialCosmos/SocialRouts')
+const ErrorHandler = require("./APIFeatures/ErrorHandler");
+const rateLimit = require("express-rate-limit");
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  next();
+});
+
+const limiter = rateLimit({
+  max: 200,
+  windowMs:60*60*1000,    // Тут указываем время через которое лимит обновится
+  message:"Too many requests, try again later" // сообщение в случае превышения лимита
+});
+app.use('/',limiter)
+app.all('*', (req, res,next) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  next(new ErrorHandler(`Url with this path ${req.originalUrl} doesnt exist`),404);
+})
 app.get('/', (req,res) => {
   res.end('<h1>TEST</h1>')
 })

35d8101cbabe129e8f0354e37b315563246bd6ea
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-20 22:05:08 +0200

    delete next

--- index.js
+++ index.js
@@ -19,12 +19,6 @@ const socialRouter = require('./Routers/SocialCosmos/SocialRouts')
 const ErrorHandler = require("./APIFeatures/ErrorHandler");
 const rateLimit = require("express-rate-limit");
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-
-});
 
 const limiter = rateLimit({
   max: 200,

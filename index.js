12394f423e4e23403f96ab701b0b394529f6dcca
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-20 22:20:38 +0200

    added CORS

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const app = express()
 
 const PORT =  process.env.PORT || 3000
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  next();
+});
 mongoose.connect("mongodb+srv://temcenkovova8:brFMAZAjzkX4ighR@cluster0.4dgfzzn.mongodb.net/natours?retryWrites=true&w=majority",{
   useNewUrlParser:true,
   useCreateIndex:true,
@@ -38,6 +43,11 @@ app.get('/', (req,res) => {
 
 app.use('/pizza',pizzaRouter)
 app.use('/social',socialRouter)
+
+app.all('*', (req, res,next) => {
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  next(new ErrorHandler(`Url with this path ${req.originalUrl} doesnt exist`),404);
+})
 app.listen(PORT, () => {
   console.log(`App running on ${PORT}`)
 })

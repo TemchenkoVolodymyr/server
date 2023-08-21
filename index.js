bbfecf199c924f613bea0da03977e009cc54038f
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-21 15:41:52 +0200

    set up error handler

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const cors = require('cors');
 const app = express()
 const helmet = require('helmet');
 const PORT =  process.env.PORT || 3000
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-//   next();
-// });
 
 app.use(cors());
 app.options('*', cors());
@@ -25,17 +19,17 @@ app.use(helmet());
 const pizzaRouter = require('./Routers/Pizza/PizzaRouts')
 const socialRouter = require('./Routers/SocialCosmos/SocialRouts')
 const ErrorHandler = require("./APIFeatures/ErrorHandler");
-// const rateLimit = require("express-rate-limit");
+const rateLimit = require("express-rate-limit");
 
 
-// const limiter = rateLimit({
-//   max: 200,
-//   windowMs:60*60*1000,
-//   message:"Too many requests, try again later"
-// });
+const limiter = rateLimit({
+  max: 500,
+  windowMs:60*60*1000,
+  message:"Too many requests, try again later"
+});
 
 
-// app.use('/',limiter)
+app.use('/',limiter)
 app.get('/', (req,res) => {
   res.end('<h1>TEST</h1>')
 })
@@ -47,11 +41,16 @@ app.all('*', (req, res,next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
   next(new ErrorHandler(`Url with this path ${req.originalUrl} doesnt exist`),404);
 })
+app.use((err,req,res,next) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || 'error';
 
-app.use(function (err,req,res,next){
-  if(err){
-    next(err)
-  }
+  res.status(err.statusCode).json({
+    status:err.status,
+    message:err.message
+  })
+
+  next()
 })
 app.listen(PORT, () => {
   console.log(`App running on ${PORT}`)

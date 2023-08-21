e55697ef19f27aa180153e5648222e5ca47a8b38
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-21 09:20:33 +0200

    cors

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const mongoose = require("mongoose");
-
+const cors = require('cors');
 const app = express()
 
 const PORT =  process.env.PORT || 3000
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+//   next();
+// });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-  next();
-});
+app.use(cors());
+app.options('*', cors());
 app.use(express.json())
 mongoose.connect("mongodb+srv://temcenkovova8:brFMAZAjzkX4ighR@cluster0.4dgfzzn.mongodb.net/natours?retryWrites=true&w=majority",{
   useNewUrlParser:true,

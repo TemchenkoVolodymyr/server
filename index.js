5e4cbc5b90b00ef0e724362f072e41f24958ed36
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-23 16:28:38 +0200

    added cors *

--- index.js
+++ index.js
@@ -9,16 +9,26 @@ const http = require('http')
 const {Server} = require('socket.io')
 const server = http.createServer(app)
 
-const io = new Server(server);
+const io = new Server(server,{
+  cors:{
+    origin:"*",
+    methods:["GET","POST","DELETE","PATCH"]
+  }
+});
 
+let onlineUsers = []
 
-io.on('connection',(socket) => {
-console.log("new connection" ,socket.id)
+io.on('connection', (socket) => {
+  console.log("new connection", socket.id)
+
+  socket.on('addNewUser', (userId) => {
+    !onlineUsers.some(user => user.userId === userId) &&
+    onlineUsers.push({
+      userId,
+      socketId: socket.id
+    });
+  })
 })
-
-
-
-
 
 
 app.use(corss());
@@ -50,7 +60,7 @@ app.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
   next(new ErrorHandler(`Url with this path ${req.originalUrl} doesnt exist`), 404);
 })
-console.log("My port is ",PORT)
+console.log("My port is ", PORT)
 server.listen(PORT, () => {
   console.log(`App running on ${PORT}`)
 })

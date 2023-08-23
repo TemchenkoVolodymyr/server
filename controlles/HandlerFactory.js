6074284b587baeac315f118bb539bcabc31507f5
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-23 21:46:08 +0200

    s

--- controlles/HandlerFactory.js
+++ controlles/HandlerFactory.js
@@ -48,9 +48,10 @@ exports.getAllHandler = Model => (async (req, res, next) => {
   let filter = {};
   if (req.params.tourId) filter = { tour: req.params.tourId };
   if(req.params.id) filter = {idUser:req.params.id}
-  if(req.query.id1) filter = {recipientId : req.query.id1}
-  if(req.query.id2) filter = {idUser:req.query.id2}
+  if(req.query.id1 && req.query.id2) filter = {recipientId : req.query.id1,idUser:req.query.id2}
+  // if(req.query.id2) filter = {idUser:req.query.id2}
 
+  console.log(filter)
   const documents = new RequestFeatures(Model.find(filter), req.query).filter().sort().fields().pagination();
   console.log("DOCUMENTS",documents)
   let doc = await documents.query

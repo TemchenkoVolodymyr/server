a02180f4cc4a54b86121ac47d481f9a5b1df49d7
TemchenkoVolodymyr <vovatemcenko4@gmail.com>
2023-08-23 15:12:36 +0200

    test

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ const mongoose = require("mongoose");
 const cors = require('cors');
 const app = express()
 const helmet = require('helmet');
-const PORT =  process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
+
+const server = require('http').createServer(app)
+const io = require('socket.io')(server);
+io.on('connection', () =>{})
+
+
+
+
+
 
 app.use(cors());
 app.options('*', cors());
 app.use(express.json())
-mongoose.connect("mongodb+srv://temcenkovova8:brFMAZAjzkX4ighR@cluster0.4dgfzzn.mongodb.net/natours?retryWrites=true&w=majority",{
-  useNewUrlParser:true,
-  useCreateIndex:true,
-  useFindAndModify:false,
+mongoose.connect("mongodb+srv://temcenkovova8:brFMAZAjzkX4ighR@cluster0.4dgfzzn.mongodb.net/natours?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 }).then(() => console.log('DB connection successful'));
 
 app.use(helmet());
@@ -22,18 +31,18 @@ const messageRouter = require('./Routers/SocialCosmos/Messages/MessagesRouts')
 const ErrorHandler = require("./APIFeatures/ErrorHandler");
 
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.end('<h1>TEST</h1>')
 })
 
-app.use('/pizza',pizzaRouter)
-app.use('/social',socialRouter)
-app.use('/message',messageRouter)
+app.use('/pizza', pizzaRouter)
+app.use('/social', socialRouter)
+app.use('/message', messageRouter)
 
-app.all('*', (req, res,next) => {
+app.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-  next(new ErrorHandler(`Url with this path ${req.originalUrl} doesnt exist`),404);
+  next(new ErrorHandler(`Url with this path ${req.originalUrl} doesnt exist`), 404);
 })
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`App running on ${PORT}`)
 })
